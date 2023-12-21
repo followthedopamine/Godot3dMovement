ea1d97edc9087b709ffa4dc5749c39660cd52342
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2023-12-21 17:12:30 +1300

    Added an animation to red button on press

--- HideButton.gd
+++ HideButton.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@onready var animation_player = get_node("../../RedButtonAnimationPlayer")
+
 func interact():
 	var button = get_parent()
-	button.set_visible(false)
+	animation_player.play("ButtonPress")
